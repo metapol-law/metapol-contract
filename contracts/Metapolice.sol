6b51377afb6cfb3976f0dc10527b8e9000949572
metapol-law-ceo <ceo@metapol.law>
2022-08-18 09:46:39 -0500

    Update Metapolice.sol

--- contracts/Metapolice.sol
+++ contracts/Metapolice.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 /// @custom:security-contact security@metapol.law
-contract Metapolice is ERC20 {
+contract Metapolice is ERC20, ERC20Burnable {
     constructor() ERC20("METAPOLICE", "METAPOLICE") {
         _mint(msg.sender, 10000000000 * 10 ** decimals());
     }
